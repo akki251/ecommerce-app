5d1f6bc7997b176358b688fee56e89f7ed52d34c
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-06-10 22:34:41 +0530

    preparing for deloy

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import AllProducts from './components/Products/AllProducts';
 import ProductDetail from './components/Products/ProductDetail';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import CreateProduct from './components/Products/CreateProduct';
 import './index.css';
 import Cart from './components/Cart/Cart';
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route path="/" exact component={AllProducts} />
+        <Route path="/" exact component={() => <Redirect to="/products" />} />
         <Route path="/products" exact component={AllProducts} />
         <Route path="/products/create" exact component={CreateProduct} />
         <Route path="/cart" exact component={Cart} />
