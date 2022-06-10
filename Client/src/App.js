8a11b7c4cccf34aa32e44be7840896aa33af7dff
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-06-10 22:08:36 +0530

    preparing for deloy

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import AllProducts from './components/Products/AllProducts';
 import ProductDetail from './components/Products/ProductDetail';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import CreateProduct from './components/Products/CreateProduct';
 import './index.css';
 import Cart from './components/Cart/Cart';
