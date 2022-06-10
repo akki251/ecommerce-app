bd7cf4f60bf34ffc8dfe8978a190f6505dfcee71
Akshansh Shrivastava <Akshansh773@gmail.com>
2022-06-10 21:49:08 +0530

    preparing for deloy

---
+++ Client/build/precache-manifest.60522211b683db7702eb6af898299b46.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9b95b43f312e453020676a91391683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6447c6c58dcb9bb00c19",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "ce6610a4c5eea2179c58",
+    "url": "/static/css/main.6666641b.chunk.css"
+  },
+  {
+    "revision": "6447c6c58dcb9bb00c19",
+    "url": "/static/js/2.7dcac9cd.chunk.js"
+  },
+  {
+    "revision": "d035ca9b412ae5749d6059c17c4cd515",
+    "url": "/static/js/2.7dcac9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c60efa7a51e1acb5ed5",
+    "url": "/static/js/3.8c77ad36.chunk.js"
+  },
+  {
+    "revision": "ce6610a4c5eea2179c58",
+    "url": "/static/js/main.565dd980.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.565dd980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5836832ceb6642a5c846",
+    "url": "/static/js/runtime-main.8a4c72dd.js"
+  }
+]);
